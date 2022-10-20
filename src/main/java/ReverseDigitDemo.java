6f0c6118c164fd1c9256ad87434f8d1529b4cb7c
Ketki Keni <sketki220@gmail.com>
2022-10-20 16:16:44 +0530

    written logic to reverse the number

--- src/main/java/ReverseDigitDemo.java
+++ src/main/java/ReverseDigitDemo.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ReverseDigitDemo {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int number = scanner.nextInt();
+        int reverse = 0;
+        while (number != 0){
+            int digit = number % 10;
+            reverse = (reverse * 10) + digit;
+            number = number / 10;
+        }
+        System.out.println("Reversed Number : " + reverse);
     }
 }
